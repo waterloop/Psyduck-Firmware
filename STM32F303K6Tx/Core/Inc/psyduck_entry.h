2944fc34d36fcb2b710e3488149781175a8395c2
Nushaab Syed <nushaab0202@gmail.com>
2022-07-07 11:25:35 -0400

    Updated psyduck_entry.h engage/disengage brakes macro to reverse the current behaviour

--- STM32F303K6Tx/Core/Inc/psyduck_entry.h
+++ STM32F303K6Tx/Core/Inc/psyduck_entry.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#define DISENGAGE_BRAKES()      (HAL_GPIO_WritePin(CONTROL_GPIO_Port, CONTROL_Pin, GPIO_PIN_RESET))
-#define ENGAGE_BRAKES()         (HAL_GPIO_WritePin(CONTROL_GPIO_Port, CONTROL_Pin, GPIO_PIN_SET))
+#define DISENGAGE_BRAKES()      (HAL_GPIO_WritePin(CONTROL_GPIO_Port, CONTROL_Pin, GPIO_PIN_SET))
+#define ENGAGE_BRAKES()         (HAL_GPIO_WritePin(CONTROL_GPIO_Port, CONTROL_Pin, GPIO_PIN_RESET))
 
 void _initialize_can();
 void psyduck_entry();
